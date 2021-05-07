6d8962942f4e22f6af19be7d2686bf5f65c9993d
legendkkutu <80611506+legendkkutu@users.noreply.github.com>
2021-05-07 12:10:51 +0900

    최신 코드 업데이트
    구글 로그인 시 underfined가 붙는 오류 해결.
    해당 커밋은 끄투닷플러스의 약간을 사용하였습니다.

--- Server/lib/Web/auth/auth_google.js
+++ Server/lib/Web/auth/auth_google.js
@@ -1,5 +1,14 @@
 const config = require('../../sub/auth.json');
 
+const isFalsyLastName = (name) => {
+    if (name === undefined || name === null ||
+        name === '' || name === ' ') {
+        return true;
+    }
+
+    return false;
+}
+
 module.exports.config = {
     strategy: require('passport-google-oauth2').Strategy,
     color: '#FFFFFF',
@@ -13,16 +22,23 @@ module.exports.strategyConfig = {
     clientSecret: config.google.clientSecret, // 이 방법을 사용하는 것을
     callbackURL: config.google.callbackURL, // 적극 권장합니다.
     passReqToCallback: true,
-    scope: ['profile', 'email', 'https://www.googleapis.com/auth/plus.login']
+    scope: ['profile', 'email', 'https://www.googleapis.com/auth/plus.login'],
+    authorizationURL: config.google.authURL
+}
+
+module.exports.authConfig = {
+    scope: ['profile', 'email']
 }
 
 module.exports.strategy = (process, MainDB, Ajae) => {
     return (req, accessToken, refreshToken, profile, done) => {
         const $p = {};
+        const lastName = profile.name.familyName + ' ';
+        const firstName = profile.name.givenName;
 
         $p.authType = "google";
         $p.id = profile.id;
-        $p.name = (profile.name.familyName != '' ? profile.name.familyName+' ' : '')+profile.name.givenName;
+        $p.name = (isFalsyLastName(profile.name.familyName) ? '' : lastName) + firstName;
         $p.title = profile.nickname;
         $p.image = profile.photos[0].value;
 
